df69640cc0cfc65c0eb49cd390ef2a1bff337f83
Alex Males <alexandru.males@gmail.com>
2019-05-24 08:24:34 +0300

    Retrieved destination account and amount from subscription eth log

--- src/main/java/malex/argent/watcher/Web3Service.java
+++ src/main/java/malex/argent/watcher/Web3Service.java
@@ -1,25 +1,39 @@
 package malex.argent.watcher;
 
+import io.reactivex.disposables.Disposable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.websocket.WebSocketClient;
 import org.web3j.protocol.websocket.WebSocketService;
+import org.web3j.utils.Numeric;
 
 import javax.annotation.PostConstruct;
 import java.net.ConnectException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class Web3Service {
 
-    private final Logger log = LoggerFactory.getLogger(Web3Service.class);
+    private final Logger logger = LoggerFactory.getLogger(Web3Service.class);
 
     private Web3j web3j;
 
     private WebSocketService webSocketService;
+
+    private Disposable subscriptionDisposable;
+
+    private List<String> addresses = Arrays.asList(
+            "0x101848D5C5bBca18E6b4431eEdF6B95E9ADF82FA",
+            "0xF6fF95D53E08c9660dC7820fD5A775484f77183A",
+            "0x7E0480Ca9fD50EB7A3855Cf53c347A1b4d6A2FF5"
+    );
 
     @PostConstruct
     private void init() throws URISyntaxException {
@@ -30,11 +44,30 @@ public class Web3Service {
 
     public void start() throws ConnectException {
         webSocketService.connect();
-        log.info("Connected to infura wss");
+        logger.info("Connected to infura wss");
+
+        subscribe();
+    }
+
+    private void subscribe() {
+        EthFilter filter = new EthFilter(null, null, addresses);
+        subscriptionDisposable = web3j.ethLogFlowable(filter).subscribe(this::handleEthLogEvent);
+        logger.info("Subscribed to ETH Log");
+    }
+
+    private void handleEthLogEvent(Log log) {
+        String toAddress = log.getTopics().get(2);
+        String strInHex = log.getData().substring(34, 66);
+        String result = Numeric.toBigInt(strInHex).toString();
+        String txHash = log.getTransactionHash();
+
+        logger.info("ETH Log Event. Contract: {}, Tx: {}", log.getAddress(), txHash);
+        logger.info("To: {}, Amount (wei): {}", toAddress, result);
     }
 
     public void stop() {
-        log.info("Shutting down wss");
+        logger.info("Shutting down wss");
+        subscriptionDisposable.dispose();
         web3j.shutdown();
     }
 }

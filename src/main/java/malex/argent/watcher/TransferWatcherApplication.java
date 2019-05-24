6c21943324b02bfde819f1edc3a6d83fe071e8ab
Alex Males <alexandru.males@gmail.com>
2019-05-24 07:01:56 +0300

    initialized as web app

---
+++ src/main/java/malex/argent/watcher/TransferWatcherApplication.java
@@ -0,0 +1,12 @@
+package malex.argent.watcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransferWatcherApplication.class, args);
+    }
+}
